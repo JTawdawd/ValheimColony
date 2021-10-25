246c9f12fa4d46c03ccd9081cd3b7be281e27ec4
Jacob Gonos <Jacobgonos@gmail.com>
2021-10-24 21:00:02 -0400

    New Honey Now Available
    In our pursuit to learn to mod valheim, I have made a new honey. This new honey is now avaiable at your nearest bed. Any bed that is, bed are also now free range. Have fun and honey resposibly.

--- JotunnModStub/ValheimColony.cs
+++ JotunnModStub/ValheimColony.cs
@@ -1,24 +1,26 @@
-﻿// JotunnModStub
-// a Valheim mod skeleton using Jötunn
-// 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
-
-using BepInEx;
+﻿using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
+using UnityEngine;
+using HarmonyLib;
+using Jotunn.Utils;
 
 namespace JotunnModStub
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class ValheimColony : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.ValheimColony";
+        public const string PluginName = "ValheimColony";
         public const string PluginVersion = "0.0.1";
-        
+        private readonly Harmony harmony = new Harmony("ValheimColony");
+
+        AssetBundle testBundle;
+        GameObject newhoneyPrefab;
+        CustomItem newhoney;
+
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
         public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
@@ -31,7 +33,11 @@ namespace JotunnModStub
             
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("ModStub has landed");
-            
+
+            harmony.PatchAll();
+
+            LoadAssets();
+
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
@@ -46,6 +52,36 @@ namespace JotunnModStub
 
             // This code runs after Valheim's FejdStartup.Awake
             Jotunn.Logger.LogInfo("FejdStartup has awoken");
+        }
+
+        void LoadAssets()
+        {
+            testBundle = AssetUtils.LoadAssetBundle("JotunnModStub/Assets/newhoney");
+            newhoneyPrefab = testBundle.LoadAsset<GameObject>("Honey 1");
+            newhoney = new CustomItem(newhoneyPrefab, fixReference: false, new Jotunn.Configs.ItemConfig
+            {
+                Amount =1,
+                Name = "New Honey",
+                Description = "Jordan Stinks"
+            });
+
+            ItemManager.Instance.AddItem(newhoney);
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(Bed), nameof(Bed.Interact))]
+        class Sleep_Patch
+        {
+            static bool Prefix(Humanoid human, bool repeat, bool alt, Transform ___m_spawnPoint)
+            {
+                human.AttachStart(___m_spawnPoint, null, hideWeapons: true, isBed: true, onShip: false, "attach_bed", new Vector3(0f, 0.5f, 0f));
+                Instantiate(ItemManager.Instance.GetItem("Honey 1").ItemPrefab, ___m_spawnPoint.position, Quaternion.identity);
+                return false;
+            }
         }
     }
 }
